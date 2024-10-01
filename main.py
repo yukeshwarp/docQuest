4d90251d3a13e7ed476b47eb36341b9f1698f35a
yukeshwarp <Yukeshwarp@docu3c.com>
2024-10-01 20:03:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ if st.session_state.document_data:
                 st.subheader("Chats", divider="orange")
                 for chat in st.session_state.chat_history:
                     # ChatGPT-like alignment: user input on the right, assistant response on the left
-                    user_chat = f"<div style='text-align: right;background-color: #1d0d47; margin: 5px;'><strong>You:</strong> {chat['question']}</div>"
-                    assistant_chat = f"<div style='text-align: left;background-color: #1d0d47; margin: 5px;'><strong>Finds:</strong> {chat['answer']}</div>"
+                    user_chat = f"<div style='text-align: right;background-color: #1d0d47;margin: 5px; border-radius: 8px; padding: 10px;'><strong>You:</strong> {chat['question']}</div>"
+                    assistant_chat = f"<div style='text-align: left;background-color: #1d0d47;margin: 5px; border-radius: 8px; padding: 10px;'><strong>Finds:</strong> {chat['answer']}</div>"
                     st.markdown(user_chat, unsafe_allow_html=True)
                     st.markdown(assistant_chat, unsafe_allow_html=True)
                     st.markdown("---")
